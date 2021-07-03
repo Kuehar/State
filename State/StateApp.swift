9dac83a8f48b311bc39c7eb4c659956a395c8da3
kuehar <green95251@gmail.com>
2021-07-03 18:09:00 +0900

    Initial Commit

---
+++ State/StateApp.swift
@@ -0,0 +1,17 @@
+//
+//  StateApp.swift
+//  State
+//
+//  Created by kuehar on 2021/07/03.
+//
+
+import SwiftUI
+
+@main
+struct StateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
